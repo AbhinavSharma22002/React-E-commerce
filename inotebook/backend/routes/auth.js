cd9c4ee56951a927e98704d2306754605d3179ae
Abhinav Sharma <jiofi22002@gmail.com>
2021-10-19 15:30:47 +0530

    hel

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const JWT_secret = 'abhinavisagood$body';
 
 //Create a user using : Post "/auth/createUser". Doesn't require Auth
 router.post(
@@ -11,7 +15,7 @@ router.post(
     body("email", "Enter valid Email").isEmail(),
     body("password", "Enter valid Password").isLength({ min: 5 }),
   ],
-  async (req, res) => {
+  (req, res) => {
     //If there are errors, return bad request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -19,30 +23,39 @@ router.post(
     }
 
     //Check whether email exist already
-    let user  = User.findOne({email: {$gte:req.body.email} }, function (err, docs) {
-      if (err){
-          console.log(err)
-      }
-      else{
-        if(docs!=null){
-          return res.status(400).json({error:'Sorry a user with the same email exist already'});
+    let user = User.findOne(
+      { email: { $gte: req.body.email } },
+      function (err, docs) {
+        if (err) {
+          console.log(err);
+        } else {
+          if (docs != null) {
+            return res
+              .status(400)
+              .json({
+                error: "Sorry a user with the same email exist already",
+              });
+          } else {
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hash = bcrypt.hashSync(req.body.password, salt);
+            user = User.create({
+              name: req.body.name,
+              password: hash,
+              email: req.body.email,
+            });
+
+            const data = {
+              user:{
+                id: user.id
+              }
+            }
+            const authData = jwt.sign(data,JWT_secret);
+            return res.json({authData});
+
+          }
         }
-        else{
-          user =  User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-          });
-          return res.status(200).json({sucess:'success'});
-        }
       }
-  });
-    
-    //   .then((user) => res.json(user))
-    //   .catch((err) => {
-    //     console.log(err);
-    //     res.json({ message: "Please enter a unique value of email" });
-    //   });
+    );
   }
 );
 
